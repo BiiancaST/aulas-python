7b32437abeb08c4913ec032e25830bb5809462d0
BiiancaST <119196951+BiiancaST@users.noreply.github.com>
2022-12-01 22:44:49 -0300

    Delete DADO.py

--- DADO.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-print('-*-'*20)
-print('BEM VINDO JOGADOR')
-print('-*-'*20)
-escolha = int(input('escolha seu dado: '))
-dado = randint(0,escolha)
-print(f'O dado escolhido foi D{escolha} e o numero sorteado foi {dado}')
